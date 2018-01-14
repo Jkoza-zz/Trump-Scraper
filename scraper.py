0ceb966a1a6321afab6947b9740f6ff31b4a742a
Jkoza <jlkoza@uwaterloo.ca>
2018-01-14 15:05:26 -0500

    more changes

--- scraper.py
+++ scraper.py
@@ -1,6 +1,4 @@
 from twython import Twython, TwythonError
-import requests
-import urllib2
 import csv
 import os
 import time
@@ -9,28 +7,26 @@ import json
 import re
 import time
 import sys
-from bs4 import BeautifulSoup
 import dateutil.parser
 
-fetch_count = 150
-APP_KEY = 'KGWjAFUIvPL1AAMWrVHZxI0sW'
-APP_SECRET = 'esPOox4alt3B3pRS17N8NkzI3BDPRKgAQ1QHHX5Kbf7XTq3zIX'
-OAUTH_TOKEN = "714312642-OptKpqfFjoVSrehqvdpB3GdtRfV3SNkAZhZ44bAy"  
-OAUTH_TOKEN_SECRET = "RuIVUCbcYoIKcyXhjxP091ECIZMIL0ezoVqGZWUY6Z636"  
+fetch_count = 15000
+APP_KEY = 'w1MYjVTrfmwvjuOo2ucEhTQBA'
+APP_SECRET = 'DEGHc5bwRB0pNztbaVasLORrqFOiqxBnHIXT1j7qwRH7U0We3W'
+OAUTH_TOKEN = "952598856767700992-3gqLYyUpByKLmJAur2sTO0Y1a6I5fPg"  
+OAUTH_TOKEN_SECRET = "EkPs8IZoo5Qnt9tOjUcXSTIGNXE86QQ62G03ksmjD8VGh"  
 
-if __name__ == "__main__":
-    output_file = open(sys.argv[1], 'wb')
-    csv_writer = csv.writer(output_file, quoting=csv.QUOTE_ALL)
-    csv_writer.writerow(['Post Time', 'Tweet URL', 'Tweet', 'Retweets', 'Favourites'])
-    
-    twitter = Twython(APP_KEY, APP_SECRET)
-    auth = twitter.get_authentication_tokens()  
-    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-    twitter.verify_credentials()
+output_file = open('output.csv', 'wb')
+csv_writer = csv.writer(output_file, quoting=csv.QUOTE_ALL)
 
-    name = url.rsplit('/', 1)[1]
-    timeline = twitter.get_user_timeline(screen_name=name, count=fetch_count)
+csv_writer.writerow(['Post Time', 'Tweet URL', 'Tweet', 'Retweets', 'Favourites'])
 
-    for tweet in timeline:
-        csv_writer.writerow([int(time.mktime(dateutil.parser.parse(tweet['created_at'].encode('utf-8')).timetuple())), "https://twitter.com/statuses/" + tweet['id_str'].encode('utf-8'),
-                            tweet['text'].encode('utf-8'), tweet['retweet_count'], tweet['favorite_count']])
+twitter = Twython(APP_KEY, APP_SECRET)
+auth = twitter.get_authentication_tokens()  
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+twitter.verify_credentials()
+
+timeline = twitter.get_user_timeline(screen_name='realDonaldTrump', count=fetch_count)
+
+for tweet in timeline:
+    csv_writer.writerow([int(time.mktime(dateutil.parser.parse(tweet['created_at'].encode('utf-8')).timetuple())), "https://twitter.com/statuses/" + tweet['id_str'].encode('utf-8'), 
+                        tweet['text'].encode('utf-8'), tweet['retweet_count'], tweet['favorite_count']])
--- upload.py
+++ upload.py
@@ -7,66 +7,28 @@ import operator
 import json
 import datetime
 import dateutil.parser as parser
-import argparse
-
-def parse_arguments():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-f", "--file", help="file to upload")
-	parser.add_argument("-i", "--ip", help="ip of database")
-	parser.add_argument("-u", "--username", help="username")
-	parser.add_argument("-p", "--password", help="password")
-	parser.add_argument("-db", "--database", help="database")
-	parser.add_argument("-c", "--collection", help="collection")
-
-	return parser.parse_args()
-
-def configure_db(arguments):
-	host = str(arguments.ip)
-	username = str(arguments.username)
-	password = str(arguments.password)
-	database = str(arguments.database)
-	collection = str(arguments.collection)
-
-	client = MongoClient(host)
-	client[database].authenticate(username, password)
-  	return client[database][collection]
 
 def format_data(data):
-	if (data[5]):
-		return ({
-			'post_time' 	: datetime.datetime.fromtimestamp(float(data[0]), None), 
-			'updated_time' 	: datetime.datetime.fromtimestamp(float(data[1]), None),
-			'scraped_time' 	: datetime.datetime.fromtimestamp(float(data[2]), None), 
-			'post_url'		: data[3], 
-			'media_url'		: data[4], 
-			'article_no'	: data[5], 
-	        'poster_name'	: data[6], 
-	        'post_type'		: data[7], 
-	        'message'		: data[8], 
-	        'description'	: data[9], 
-	        'likes_count'	: data[10], 
-	        'repost_count'	: data[11], 
-	        'comments'		: data[12], 
-	        'source'		: data[13],
-	        'reach'			: data[14],
-	        'engagement'	: data[15],
-	        'user_shares'	: data[16]
-		})
+	return ({
+		'Post Time' 	: datetime.datetime.fromtimestamp(float(data[0]), None), 
+		'Tweet URL' 	: data[1],
+		'Tweet' 		: data[2], 
+		'Retweets'		: data[3], 
+		'Favourites'	: data[4]
+	})
 
-	return	{}
+client = MongoClient('localhost:27017')
+database = client['tweets']['tweets']
 
-if __name__ == "__main__":
+file = open('output.csv')
+rows = csv.reader(file,delimiter=',')
 
-	arguments = parse_arguments()
-	database = configure_db(arguments)
-	file = open(str(arguments.file))
-	rows = csv.reader(file,delimiter=',')
-	#skip the header
-	next(rows)
-	
-	for i in rows:
-		data = format_data(i)
-		if (data):
-			database.insert_one(format_data(i)).inserted_id
+#skip the header
+next(rows)
 
-	file.close()
+for i in rows:
+	data = format_data(i)
+	if (data):
+		database.insert_one(format_data(i)).inserted_id
+
+file.close()
