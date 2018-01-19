89afac4aac6a06911f7615a338e941aafc7c8b49
EC2 Default User <ec2-user@ip-172-31-39-169.us-east-2.compute.internal>
2018-01-19 21:35:10 +0000

    ec2 config

--- scrape.sh
+++ scrape.sh
@@ -2,4 +2,6 @@ logDir=/home/ec2-user/logs
 scriptDir=/home/ec2-user/trump-scraper
 
 python ${scriptDir}/scraper.py > ${logDir}/scraper.log 2>&1
+echo "scraped"
 python ${scriptDir}/upload.py > ${logDir}/scraper.log 2>&1
+echo "uploaded"
