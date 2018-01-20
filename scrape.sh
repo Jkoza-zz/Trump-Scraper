fffa737052da15647d817a724234f16cd557d0e3
EC2 Default User <ec2-user@ip-172-31-39-169.us-east-2.compute.internal>
2018-01-20 02:35:01 +0000

    fixed path

--- scrape.sh
+++ scrape.sh
@@ -1,5 +1,5 @@
 logDir=/home/ec2-user/logs
-scriptDir=/home/ec2-user/trump-scraper
+scriptDir=/home/ec2-user/Trump-Scraper
 
 python ${scriptDir}/scraper.py > ${logDir}/scraper.log 2>&1
 echo "scraped"
